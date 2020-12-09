917229f916b69589a7df8ad5e1813049035db2be
chrishj59 <chrishj@btinternet.com>
2020-12-09 10:19:24 +0000

    add fallback true to load page not defined

--- pages/portfolios/[id]/index.js
+++ pages/portfolios/[id]/index.js
@@ -1,8 +1,9 @@
 import BaseLayout from "@/components/layouts/BaseLayout";
 import BasePage from "@/components/BasePage";
 import { formatDate } from "helpers/functions";
-import { useRouter } from "next/router";
+
 import { useGetUser } from "@/actions/user";
+import { useRouter } from "next/router";
 
 import PortfolioApi from "@/lib/api/portfolios";
 
@@ -10,6 +11,9 @@ const Portfolio = ({ portfolio }) => {
 	const router = useRouter();
 	const { data: dataU, loading: loadingU } = useGetUser();
 
+	if (router.isFallback) {
+		return <h1>Loading portfolio....</h1>;
+	}
 	return (
 		<BaseLayout navclassName="transparent" user={dataU} loading={loadingU}>
 			<BasePage
@@ -20,24 +24,31 @@ const Portfolio = ({ portfolio }) => {
 				<div className="portfolio-detail">
 					<div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
 						<main role="main" className="inner page-cover">
-							<h1 className="cover-heading">{portfolio.title}</h1>
-							<p className="lead dates">
-								{formatDate(portfolio.startDate)} -{" "}
-								{formatDate(portfolio.endDate) || "Current"}
-							</p>
-							<p className="lead info mb-0">
-								{portfolio.jobTitle} | {portfolio.company} |{" "}
-								{portfolio.location}
-							</p>
-							<p className="lead">{portfolio.description}</p>
-							<p className="lead">
-								<a
-									href={portfolio.companyWebsite}
-									target="_"
-									className="btn btn-lg btn-secondary">
-									Visit Company
-								</a>
-							</p>
+							{router.isFallback && (
+								<h1 className="cover-heading">'Loading portfolio ....'</h1>
+							)}
+							{!router.isFallback && (
+								<>
+									<h1 className="cover-heading">{portfolio.title}</h1>
+									<p className="lead dates">
+										{formatDate(portfolio.startDate)} -{" "}
+										{formatDate(portfolio.endDate) || "Current"}
+									</p>
+									<p className="lead info mb-0">
+										{portfolio.jobTitle} | {portfolio.company} |{" "}
+										{portfolio.location}
+									</p>
+									<p className="lead">{portfolio.description}</p>
+									<p className="lead">
+										<a
+											href={portfolio.companyWebsite}
+											target="_"
+											className="btn btn-lg btn-secondary">
+											Visit Company
+										</a>
+									</p>
+								</>
+							)}
 						</main>
 					</div>
 				</div>
@@ -67,7 +78,7 @@ export async function getStaticPaths() {
 	});
 
 	// fallback:false  = not found pages will be resolved into 404 page
-	return { paths, fallback: false };
+	return { paths, fallback: true };
 }
 
 export async function getStaticProps({ params }) {
